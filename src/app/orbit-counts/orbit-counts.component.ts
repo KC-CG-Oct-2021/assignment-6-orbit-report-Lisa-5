08db88c44ecf908952a5470c6ab57d2692498363
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-02-27 19:26:43 -0600

    coded first half of search bonus exercise

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -9,7 +9,7 @@ import { Satellite } from '../satellite';
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
-    //count: number = this.satellites.length
+
   constructor() { }
 
   ngOnInit() {
